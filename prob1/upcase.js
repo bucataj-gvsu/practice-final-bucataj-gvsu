2128dd19c9f1da809005ae8129235a46ae38e8b5
Jason Bucata <bucataj@mail.gvsu.edu>
2020-04-15 15:29:22 -0400

    Problem 1: Unbotch document.styleSheets camel case

--- prob1/upcase.js
+++ prob1/upcase.js
@@ -1,11 +1,10 @@
 function addCSSRule(className, xform) {
   // from: https://gomakethings.com/two-ways-to-set-an-elements-css-with-vanilla-javascript/
   // from: https://love2dev.com/blog/css-text-transform/
-  document.stylesheets[0].insertRule(`.${className} { text-transform: ${xform} }`, 0);
+  document.styleSheets[0].insertRule(`.${className} { text-transform: ${xform} }`, 0);
 }
 
 function upYourCases() {
-  console.log("Clicked!");
   addCSSRule("allcaps", "uppercase");
   addCSSRule("upcase", "capitalize");
 }
