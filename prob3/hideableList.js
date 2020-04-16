cdcb3cabc29aae71a0839dbf3108d976b34f444e
Jason Bucata <bucataj@mail.gvsu.edu>
2020-04-15 21:12:21 -0400

    Twistable triangles

--- prob3/hideableList.js
+++ prob3/hideableList.js
@@ -9,28 +9,54 @@ function hasClass(element, className) {
 
 function hider(hl) {
   let targetList = hl.children;
+
+  let saveHeader = undefined;
+  for (let i = 0; i < targetList.length; i++) {
+    let target = targetList[i];
+    if (hasClass(target, "hl-header")) {
+      saveHeader = target;
+      break;
+    }
+  }
+
+  let disappearing = /^\u25BC/.test(saveHeader.innerText);
+
+  // twist the triangle
+  if (disappearing) {
+    saveHeader.innerText = saveHeader.innerText.replace('\u25BC', '\u25B6');
+  } else {
+    saveHeader.innerText = saveHeader.innerText.replace('\u25B6', '\u25BC');
+  }
+
   for (let i = 0; i < targetList.length; i++) {
     let target = targetList[i];
     if (!hasClass(target, "hl-header")) {
-      if (target.style.display === "none") {
-        target.style.display = "";
-      } else if (target.style.display === "") {
+      // change the visibility
+      if (disappearing) {
         target.style.display = "none";
       } else {
-        console.log(`Unexpected display value ${target.style.display}`);
-        console.log(target);
+        target.style.display = "";
       }
     }
   }
+
 }
 
 function makeHideables() {
-  targets = document.getElementsByClassName("hideableList");
+  hideables = document.getElementsByClassName("hideableList");
 
-  for (let i = 0; i < targets.length; i++) {
-    let target = targets[i];
+  for (let i = 0; i < hideables.length; i++) {
+    let target = hideables[i];
     target.onclick = () => { hider(target) };
   }
+
+  twistables = document.getElementsByClassName("hl-header");
+
+  for (let i = 0; i < twistables.length; i++) {
+    let target = twistables[i];
+    target.innerText = '\u25B6' + target.innerText;
+  }
+
 }
 
 makeHideables();
